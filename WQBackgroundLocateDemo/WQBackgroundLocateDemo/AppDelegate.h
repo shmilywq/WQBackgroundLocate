3f482a16edd395acfdb69c3e4d3ce794c29f3169
wangqiang <wangqiang@keking.cn>
2017-08-10 10:32:40 +0800

    创建项目

---
+++ WQBackgroundLocateDemo/WQBackgroundLocateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WQBackgroundLocateDemo
+//
+//  Created by shmily on 2017/8/10.
+//  Copyright © 2017年 shmily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
